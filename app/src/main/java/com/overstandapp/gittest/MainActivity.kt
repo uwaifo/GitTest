bd75704479465fc5643ca9b9d3eecb5f8507c478
uwaifo <mr.uwaifo@gmail.com>
2018-06-24 08:16:53 +0100

    Initial commit

---
+++ app/src/main/java/com/overstandapp/gittest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.overstandapp.gittest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
